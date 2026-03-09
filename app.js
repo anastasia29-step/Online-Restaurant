bc09d632fc9a2671b1e6586c770595afaea38f50
anastasia29-step <stephaniataso@gmail.com>
2026-03-09 22:05:11 +0400

    products

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let ul = document.querySelector("ul")
+let right = document.querySelector(".right")
 
 fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
     .then((pasuxi) => pasuxi.json())
@@ -11,9 +12,36 @@ let sectionCard = document.querySelector(".sectionCard")
 let left = document.querySelector(".left")
 
 fetch("https://restaurant.stepprojects.ge/api/Products/GetFiltered")
-.then(pasuxi => pasuxi.json())
-.then(data => console.log(data)
-)
+    .then(pasuxi => pasuxi.json())
+    .then(data => console.log(data)
+    )
+fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+    .then(pasuxi => pasuxi.json())
+    .then((data) => {
+        data.forEach((item) => right.innerHTML += card(item))
+    })
+
+function card(item) {
+    return ` <div class="card">
+                <img src="${item.image}" alt="">
+                <h1>${item.name}</h1>
+                <p>Spiciness: ${item.spiciness}</p>
+                <div class="sides">
+                    <div class="checks">
+                        <input type="checkbox" (${item.nuts} ? "checked" : "")>
+                        <p>Nuts</p>
+                    </div>
+                    <div class="checks">
+                        <input type="checkbox">
+                        <p>Vegeterian</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <h1>$ </h1>
+                    <button>Add to cart</button>
+                </div>
+            </div>`
+}
 
 
 
