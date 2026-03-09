25f7b809cdab1e37a56c69fd49110ee947d4bd2e
anastasia29-step <stephaniataso@gmail.com>
2026-03-09 14:45:10 +0400

    change

--- app.js
+++ app.js
@@ -7,65 +7,8 @@ fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
         data.forEach((item) => ul.innerHTML += `<li onclick="changeCategory(${item.id})">${item.name}</li>`)
     })
 
-function showAll() {
-    section.innerHTML = ""
-    fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
-        .then((pasuxi) => pasuxi.json())
-        .then((data) => data.forEach((item) => section.innerHTML += card(item)))
-        .catch(() => section.innerHTML = "<h1> კავშირის პრობლემა </h1>")
-}
-
-showAll()
 
 
-function changeCategory(id) {
-    section.innerHTML = ""
-    fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`)
-        .then((pasuxi) => pasuxi.json())
-        .then((data) => data.products.forEach((item) => section.innerHTML += card(item)))
-}
-
-function card(item) {
-    return `<div class="card">
-            <img src="${item.image}" alt="">
-            <h4>${item.name}</h4>
-            <h6>${item.price}₾</h6>
-            <button onclick="addToCart(${item.id}, ${item.price})">ADD TO CART</button>
-        </div>`
-}
 
 
-function addToCart(id, fasi) {
-    let info = {
-        quantity: 1,
-        price: fasi,
-        productId: id
-    }
 
-    fetch("https://restaurant.stepprojects.ge/api/Baskets/AddToBasket", {
-        method: "POST",
-        headers: {
-            accept: "text/plain",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(info)
-    }).then((pasuxi) => pasuxi.json())
-        .then((data) => {
-           let existing = item.product.id
-           if(existing){
-            fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket", {
-                method: "PUT",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    id: existing.id,
-                    quantity: existing.quantity +1
-                })
-            })
-
-           } 
-
-        })
-
-}
